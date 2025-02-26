01ff4374a3d838a26eef3244341e00e24f0e44b8
MelanieKK <koroljovamelanie512@gmail.com>
2025-02-26 11:38:31 +0200

    datatyp

--- Datatypes/Program.cs
+++ Datatypes/Program.cs
@@ -13,6 +13,25 @@
             //komaga number ??
             double comaNumber = 12.5;
             Console.WriteLine(comaNumber);
+
+            //tähemärk
+            string stringVar = ""asd456&%""2e990";
+            Console WriteLine(stringVar)
+
+            float floatNumber = 12.5f;
+            Console.WriteLine(floatNumber);
+
+            char charVarible = '2';
+            Console.WriteLine(charVarible);
+
+            bool boolVarible = true;
+            Console.WriteLine(boolVarible);
+                
+
+                
+            
+
         }
+       
     }
 }
